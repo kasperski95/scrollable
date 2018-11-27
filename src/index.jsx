14608df6660a09e6c7c816db2860f7a54c441154
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2018-11-27 19:55:55 +0100

    Add vertical scrollbar

--- src/index.jsx
+++ src/index.jsx
@@ -1,56 +1,9 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 
 var theme = {
 	gutter: 8
 }
-
-const CustomScrollbarWrapper = styled.div`
-	width: ${theme.gutter*2}px;
-	height: 100%;
-	background-color: #0D0;
-	position: absolute;
-	top: 0;
-	right: 0;
-	z-index: 90;
-`;
-
-const CustomScrollbarFiller = styled.div`
-	width: 100%;
-	height: 100%;
-	background-color: #00D;
-	position: relative;
-`;
-
-const CustomScrollbarElement = styled.div`
-	width: 100%;
-	height: ${props => props.height || "100%"};
-	background-color: #0D0;
-	position: absolute;
-	top: 0;
-	left: 0;
-`;
-
-const Content = styled.div`
-	width: ${props => props.width || "100%"};
-	height: ${props => props.height || "100%"};
-	overflow: scroll;
-	position: relative;
-`;
-
-const ScrollWrapper = styled.div`
-	width: ${props => props.width || "100%"};
-	height: ${props => props.height || "100%"};
-	position: relative;
-	overflow: hidden;
-`;
-
-const DefaultScrollHider = styled.div`
-	width: ${props => props.width || "100%"};
-	height: ${props => props.height || "100%"};
-	overflow: hidden;
-`;
-
 
 
 export default class Scrollbar extends Component {
@@ -58,17 +11,26 @@ export default class Scrollbar extends Component {
 		super();
 		this.dragEnabled = false;
 		this.prevMouseDownPosY = 0;
+		this.scrollbarWidth = 4;
+		this.scrollbarCaptureAreaWidth = 8;
 
+		// boilerplate
 		this.Wrapper = React.createRef();
 		this.Content = React.createRef();
-		this.Thumb = React.createRef();
+		this.ThumbY = React.createRef();
 		this.Dummmy = React.createRef();
-
 		this.handleScroll = this.handleScroll.bind(this);
 		this.handleOnMouseDown = this.handleOnMouseDown.bind(this);
 		this.handleOnMouseMove = this.handleOnMouseMove.bind(this);
 		this.handleOnMouseUp = this.handleOnMouseUp.bind(this);
 		this.handleOnDragStart = this.handleOnDragStart.bind(this);
+	}
+
+	componentDidMount () {
+		if (this.Content.current.offsetHeight == this.Content.current.scrollHeight + this.findScrollbarWidth()) {
+			this.ThumbY.current.parentElement.style.opacity = 0;
+		}
+		this.handleScroll();
 	}
 
 	findScrollbarWidth() {
@@ -84,71 +46,84 @@ export default class Scrollbar extends Component {
 		return 100 - dummy.getBoundingClientRect().width;
 	}
 
-	handleScroll(e) {	
-		let scrollbarWidth = this.findScrollbarWidth();
-		const Content = this.Content.current;
-		const Thumb = this.Thumb.current;
-		const ContentParent = Content.parentElement;
+	isOtherScrollbarActivated() {
+		let dummy = document.getElementById('scrollbarActiveDummy');
+		if (!dummy) {
+			
+			return false;
+		}		
+		return true;
+	}
 
-		// hide default scrollbars
+	handleScroll(e) {	
+		const scrollbarWidth = this.findScrollbarWidth();	
+		const Wrapper = this.Wrapper.current;
+		const Content = this.Content.current;
+		const ThumbY = this.ThumbY.current;
+		const ContentParent = Content.parentElement;
 		
-		Content.style.width = `${ContentParent.offsetWidth + scrollbarWidth}px`;
+
+		// hide default scrollbars	
+		Content.style.width = `${ContentParent.offsetWidth + scrollbarWidth}px`;	
+
+		if (!this.props.height)
+			Wrapper.style.height = `${Content.offsetHeight - scrollbarWidth}px`;
 		Content.style.height = `${ContentParent.offsetHeight + scrollbarWidth}px`;
 		
-		// update thumb height and position
-		Thumb.style.height = `${Content.clientHeight / Content.scrollHeight * 100}%`;
-		Thumb.style.top = `${Content.scrollTop / Content.scrollHeight * 100}%`;
+		// update ThumbY height and position
+		ThumbY.style.height = `${Content.clientHeight / Content.scrollHeight * 100}%`;
+		ThumbY.style.top = `${Content.scrollTop / Content.scrollHeight * 100}%`;
 	}
-
-	componentDidMount () {
-		this.handleScroll();
-	}
-
-
 
 	handleOnMouseDown(e) {
-		e.preventDefault();
-		console.log("mouseDown");
 		this.dragEnabled = true;
 		e.target.style.position = "fixed";
-		e.target.style.left = `0px`;
+		e.target.style.left = "0px";
+		e.target.style.width = "100%";
+
 		this.prevMouseDownPosY = e.clientY;
+		this.ThumbY.current.style.backgroundColor = "rgba(0, 0, 0, 0.75)";
 	}
 
 	handleOnMouseMove(e) {
 		if (this.dragEnabled) {
-			console.log(e.clientY);	
+			const Content = this.Content.current;
 			let delta = e.clientY - this.prevMouseDownPosY;
 			this.prevMouseDownPosY = e.clientY;
-			this.refs.Content.scrollBy(0, delta * this.refs.Content.scrollHeight / this.refs.Content.clientHeight );
+			Content.scrollBy(0, delta * Content.scrollHeight / Content.clientHeight );
 		}
 	}
 
 	handleOnMouseUp(e) {
-		console.log("mouseUp | Out");
 		this.dragEnabled = false;
 		e.target.style.position = "absolute";
+		e.target.style.left = `${-this.scrollbarCaptureAreaWidth/2}px`;
+		e.target.style.width = `${this.scrollbarCaptureAreaWidth}px`;;
+		this.ThumbY.current.style.backgroundColor = "rgba(128, 128, 128, 1)";
 	}
 
 	handleOnDragStart(e) {
 		e.preventDefault();
 	}
 
+
 	render() {
 		return (
-			<ScrollWrapper ref={this.Wrapper}>
+			<ScrollWrapper ref={this.Wrapper} width={this.props.width} height={this.props.height}>
 				<CustomScrollbarWrapper>
 					<CustomScrollbarFiller>
-
-						<div ref={this.Dummy} style={{width: "100%", height:"100%", opacity: 0, backgroundColor: "red", top: 0, position: "absolute", zIndex: 99}}
+						<Dummy ref={this.Dummy}
+							width={`${this.scrollbarCaptureAreaWidth}px`}
+							left={`${-this.scrollbarCaptureAreaWidth/2}px`}
 							onDragStart={this.handleOnDragStart}
 							onMouseDown={this.handleOnMouseDown}
-							onMouseMove={this.handleOnMouseMove}
+							onMouseMoveCapture={this.handleOnMouseMove}
 							onMouseUp={this.handleOnMouseUp}
-							onMouseLeave={this.handleOnMouseUp}
-							//onDrag={this.handleOnDrag}
-						></div> 
-						<CustomScrollbarElement ref={this.Thumb}  />
+							onMouseLeave={this.handleOnMouseUp} /> 
+						<CustomScrollbarElement ref={this.ThumbY}
+							width={`${this.scrollbarWidth}px`}
+							left={`${-(this.scrollbarCaptureAreaWidth - this.scrollbarWidth) / 2}px`}
+						/>
 					</CustomScrollbarFiller>
 				</CustomScrollbarWrapper>
 				<DefaultScrollHider>
@@ -162,3 +137,62 @@ export default class Scrollbar extends Component {
 }
 
 
+const Dummy = styled.div`
+	width: ${props => props.width || "100%"};
+	height: 100%;
+	opacity: 0;
+	background-color: red;
+	top: 0;
+	left: ${props => props.left || 0};
+	position: absolute;
+	z-index: 99;
+`;
+
+const CustomScrollbarWrapper = styled.div`
+	width: ${props => props.width || "4px"};
+	height: 100%;
+	position: absolute;
+	top: 0;
+	right: 0;
+	z-index: 90;
+`;
+
+const CustomScrollbarFiller = styled.div`
+	width: 100%;
+	height: 100%;
+	background: rgba(0,0,0,0);
+	position: relative;
+`;
+
+const CustomScrollbarElement = styled.div`
+	width: 100%;
+	height: ${props => props.height || "100%"};
+	background-color: ${props => props.color || "#888"};
+	position: absolute;
+	top: 0;
+	left: ${props => props.left || 0};
+	opacity: 1;
+	border-radius: ${props => props.radius || "4px"};
+`;
+
+const Content = styled.div`
+	width: ${props => props.width || "100%"};
+	height: ${props => props.height || "100%"};
+	overflow: scroll;
+	position: relative;
+	display: inline-block;
+`;
+
+const ScrollWrapper = styled.div`
+	width: ${props => props.width || "100%"};
+	height: ${props => props.height || "100%"};
+	position: relative;
+	overflow: hidden;
+`;
+
+const DefaultScrollHider = styled.div`
+	width: ${props => props.width || "100%"};
+	height: ${props => props.height || "100%"};
+	overflow: hidden;
+	display: inline-block;
+`;
